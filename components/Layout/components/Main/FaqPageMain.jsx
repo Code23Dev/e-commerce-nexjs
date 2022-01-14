e42b7d5b2b85eb03c3562e365e814e52732c2fef
leman ibrahimli <leman.ibrahimli23@gmail.com>
2022-01-14 12:05:36 +0400

    add faq

---
+++ components/Layout/components/Main/FaqPageMain.jsx
@@ -0,0 +1,253 @@
+import React from "react";
+
+export default function FaqPageMain(){
+    return (
+        <div>
+            <main className="main">
+                <div className="page-header" style={{height: '180px'}}>
+                    <div className="container">
+                        <h1 className="page-title mb-0">FAQs</h1>
+                    </div>
+                </div>
+                <nav className="breadcrumb-nav mb-10 pb-1">
+                    <div className="container">
+                        <ul className="breadcrumb">
+                            <li><a href="demo1.html">Home</a></li>
+                            <li>FAQs</li>
+                        </ul>
+                    </div>
+                </nav>
+                <div className="page-content faq">
+                    <div className="container">
+                        <section className="content-title-section">
+                            <h3 className="title title-simple justify-content-center bb-no pb-0">Frequent Asked
+                                Questions
+                            </h3>
+                            <p className="description text-center">You can show the faqs with <b>Wolmart
+                                Elements</b> easily.</p>
+                        </section>
+
+                        <section className="mb-6">
+                            <h4 className="title title-center mb-5">Shipping Information</h4>
+                            <div className="row">
+                                <div className="col-md-6 mb-8">
+                                    <div className="accordion accordion-bg accordion-gutter-md accordion-border">
+                                        <div className="card">
+                                            <div className="card-header">
+                                                <a href="#collapse1-1" className="collapse">What Shipping Methods are
+                                                    Available?</a>
+                                            </div>
+                                            <div id="collapse1-1" className="card-body expanded">
+                                                <p className="mb-0">Fringilla urna porttitor rhoncus dolor purus. Luctus
+                                                    venenatis lectus semper bibendum
+                                                    Diam maecenas ultricies mi eget mauris. Nibh tellus molestie nunc
+                                                    non isse faucibus
+                                                    Ultrices eros in cursus turpis massa tincidunt. Ante in nibh mauri
+                                                    eger enim neque volu
+                                                    lectus. Etiam non quam lacus suspendisse faucibus.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="card">
+                                            <div className="card-header">
+                                                <a href="#collapse1-2" className="expand">How Long Will it Take to Get
+                                                    My Package?</a>
+                                            </div>
+                                            <div id="collapse1-2" className="card-body collapsed">
+                                                <p className="mb-0">Fringilla urna porttitor rhoncus dolor purus. Luctus
+                                                    venenatis lectus semper bibendum
+                                                    Diam maecenas ultricies mi eget mauris. Nibh tellus molestie nunc
+                                                    non isse faucibus
+                                                    Ultrices eros in cursus turpis massa tincidunt. Ante in nibh mauri
+                                                    eger enim neque volu
+                                                    lectus. Etiam non quam lacus suspendisse faucibus.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-6 mb-8">
+                                    <div className="accordion accordion-bg accordion-gutter-md accordion-border">
+                                        <div className="card">
+                                            <div className="card-header">
+                                                <a href="#collapse1-3" className="collapse">Do You Ship
+                                                    Internationally?</a>
+                                            </div>
+                                            <div id="collapse1-3" className="card-body expanded">
+                                                <p className="mb-0">Fringilla urna porttitor rhoncus dolor purus. Luctus
+                                                    venenatis lectus semper bibendum
+                                                    Diam maecenas ultricies mi eget mauris. Nibh tellus molestie nunc
+                                                    non isse faucibus
+                                                    Ultrices eros in cursus turpis massa tincidunt. Ante in nibh mauri
+                                                    eger enim neque volu
+                                                    lectus. Etiam non quam lacus suspendisse faucibus.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="card">
+                                            <div className="card-header">
+                                                <a href="#collapse1-4" className="expand">What Shipping Methods are
+                                                    Available?</a>
+                                            </div>
+                                            <div id="collapse1-4" className="card-body collapsed">
+                                                <p className="mb-0">Fringilla urna porttitor rhoncus dolor purus. Luctus
+                                                    venenatis lectus semper bibendum
+                                                    Diam maecenas ultricies mi eget mauris. Nibh tellus molestie nunc
+                                                    non isse faucibus
+                                                    Ultrices eros in cursus turpis massa tincidunt. Ante in nibh mauri
+                                                    eger enim neque volu
+                                                    lectus. Etiam non quam lacus suspendisse faucibus.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </section>
+
+                        <section className="mb-10">
+                            <h4 className="title title-center mb-5">Payment</h4>
+                            <div className="row">
+                                <div className="col-md-6 mb-4">
+                                    <div className="accordion accordion-bg accordion-gutter-md accordion-border">
+                                        <div className="card">
+                                            <div className="card-header">
+                                                <a href="#collapse2-1" className="collapse">What Payment Methods are
+                                                    Accepted?</a>
+                                            </div>
+                                            <div id="collapse2-1" className="card-body expanded">
+                                                <p className="mb-0">Fringilla urna porttitor rhoncus dolor purus. Luctus
+                                                    venenatis lectus semper bibendum
+                                                    Diam maecenas ultricies mi eget mauris. Nibh tellus molestie nunc
+                                                    non isse faucibus
+                                                    Ultrices eros in cursus turpis massa tincidunt. Ante in nibh mauri
+                                                    eger enim neque volu
+                                                    lectus. Etiam non quam lacus suspendisse faucibus.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="accordion accordion-bg accordion-gutter-md accordion-border">
+                                        <div className="card">
+                                            <div className="card-header">
+                                                <a href="#collapse2-2" className="collapse">Is Buying On-Line Safe?</a>
+                                            </div>
+                                            <div id="collapse2-2" className="card-body expanded">
+                                                <p className="mb-0">Fringilla urna porttitor rhoncus dolor purus. Luctus
+                                                    venenatis lectus semper bibendum
+                                                    Diam maecenas ultricies mi eget mauris. Nibh tellus molestie nunc
+                                                    non isse faucibus
+                                                    Ultrices eros in cursus turpis massa tincidunt. Ante in nibh mauri
+                                                    eger enim neque volu
+                                                    lectus. Etiam non quam lacus suspendisse faucibus.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </section>
+
+                        <section className="mb-10">
+                            <h4 className="title title-center mb-5">Orders & Returns</h4>
+                            <div className="row">
+                                <div className="col-md-6 mb-8">
+                                    <div className="accordion accordion-bg accordion-gutter-md accordion-border">
+                                        <div className="card">
+                                            <div className="card-header">
+                                                <a href="#collapse3-1" className="collapse">How do I Place an Order?</a>
+                                            </div>
+                                            <div id="collapse3-1" className="card-body expanded">
+                                                <p className="mb-0">Fringilla urna porttitor rhoncus dolor purus. Luctus
+                                                    venenatis lectus semper bibendum
+                                                    Diam maecenas ultricies mi eget mauris. Nibh tellus molestie nunc
+                                                    non isse faucibus
+                                                    Ultrices eros in cursus turpis massa tincidunt. Ante in nibh mauri
+                                                    eger enim neque volu
+                                                    lectus. Etiam non quam lacus suspendisse faucibus.
+                                                </p>
+                                            </div>
+                                        </div>
+
+                                        <div className="card">
+                                            <div className="card-header">
+                                                <a href="#collapse3-3" className="expand">Do I Need an Account to Place
+                                                    an Order?</a>
+                                            </div>
+                                            <div id="collapse3-3" className="card-body collapsed">
+                                                <p className="mb-0">Fringilla urna porttitor rhoncus dolor purus. Luctus
+                                                    venenatis lectus semper bibendum
+                                                    Diam maecenas ultricies mi eget mauris. Nibh tellus molestie nunc
+                                                    non isse faucibus
+                                                    Ultrices eros in cursus turpis massa tincidunt. Ante in nibh mauri
+                                                    eger enim neque volu
+                                                    lectus. Etiam non quam lacus suspendisse faucibus.
+                                                </p>
+                                            </div>
+                                        </div>
+
+                                        <div className="card">
+                                            <div className="card-header">
+                                                <a href="#collapse3-5" className="expand">How Can I Return a
+                                                    Product?</a>
+                                            </div>
+                                            <div id="collapse3-5" className="card-body collapsed">
+                                                <p className="mb-0">Fringilla urna porttitor rhoncus dolor purus. Luctus
+                                                    venenatis lectus semper bibendum
+                                                    Diam maecenas ultricies mi eget mauris. Nibh tellus molestie nunc
+                                                    non isse faucibus
+                                                    Ultrices eros in cursus turpis massa tincidunt. Ante in nibh mauri
+                                                    eger enim neque volu
+                                                    lectus. Etiam non quam lacus suspendisse faucibus.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-6 mb-8">
+                                    <div className="accordion accordion-bg accordion-gutter-md accordion-border">
+                                        <div className="card">
+                                            <div className="card-header">
+                                                <a href="#collapse3-2" className="collapse">How Can I or Change My
+                                                    Order?</a>
+                                            </div>
+                                            <div id="collapse3-2" className="card-body expanded">
+                                                <p className="mb-0">Fringilla urna porttitor rhoncus dolor purus. Luctus
+                                                    venenatis lectus semper bibendum
+                                                    Diam maecenas ultricies mi eget mauris. Nibh tellus molestie nunc
+                                                    non isse faucibus
+                                                    Ultrices eros in cursus turpis massa tincidunt. Ante in nibh mauri
+                                                    eger enim neque volu
+                                                    lectus. Etiam non quam lacus suspendisse faucibus.
+                                                </p>
+                                            </div>
+                                        </div>
+
+                                        <div className="card">
+                                            <div className="card-header">
+                                                <a href="#collapse3-4" className="expand">How do I Track Order?</a>
+                                            </div>
+                                            <div id="collapse3-4" className="card-body collapsed">
+                                                <p className="mb-0">Fringilla urna porttitor rhoncus dolor purus. Luctus
+                                                    venenatis lectus semper bibendum
+                                                    Diam maecenas ultricies mi eget mauris. Nibh tellus molestie nunc
+                                                    non isse faucibus
+                                                    Ultrices eros in cursus turpis massa tincidunt. Ante in nibh mauri
+                                                    eger enim neque volu
+                                                    lectus. Etiam non quam lacus suspendisse faucibus.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </main>
+        </div>
+    )}
---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderTop2 from "../components/Layout/components/Header/HeaderTop2";
+import FaqPageMain from "../components/Layout/components/Main/FaqPageMain";
+
+export default function faq() {
+    return (
+        <div>
+            <HeaderTop2/>
+            <FaqPageMain/>
+        </div>
+    )
+}
