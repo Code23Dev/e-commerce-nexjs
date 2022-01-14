775f39da42570fc3cf42228263319a5deca11813
leman ibrahimli <leman.ibrahimli23@gmail.com>
2022-01-14 11:58:24 +0400

    add wishlist

---
+++ pages/wishlist.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderTop2 from "../components/Layout/components/Header/HeaderTop2";
+import WishlistPage from "../components/Layout/components/Main/WishlistPage";
+
+export default function wishlist() {
+    return (
+        <div>
+            <HeaderTop2/>
+            <WishlistPage/>
+        </div>
+    )
+}
